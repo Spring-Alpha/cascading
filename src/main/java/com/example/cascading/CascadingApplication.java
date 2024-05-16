a2107a486d818b194e0e9e910db81f35ff573f52
Md. Alif Babu <muhammad.alifbabu@gmail.com>
2024-05-16 13:04:39 +0600

    initial commit

---
+++ src/main/java/com/example/cascading/CascadingApplication.java
@@ -0,0 +1,13 @@
+package com.example.cascading;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CascadingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CascadingApplication.class, args);
+	}
+
+}
